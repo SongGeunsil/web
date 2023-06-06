44db8a05b911ac18a77726971ba36f92a9982278
SongGeunsil <135718420+SongGeunsil@users.noreply.github.com>
2023-06-06 23:17:41 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Todo from './todos/Todos';
+import Todos from './todos/Todos';
+
+
+
+function App() {
+  return (
+    <div>
+      <Todos />
+      
+    </div>
+  );
+}
+
+export default App;
